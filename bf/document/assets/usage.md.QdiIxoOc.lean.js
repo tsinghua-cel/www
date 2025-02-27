4b12dd8682853e5873127c459d4459f7c5c36731
root <root@144.34.190.28>
2025-02-27 12:36:22 +0000

    update content

--- bf/document/assets/usage.md.QdiIxoOc.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,c as e,o as t,ae as i}from"./chunks/framework.CflXwq_F.js";const k=JSON.parse('{"title":"Basic Usage","description":"","frontmatter":{},"headers":[],"relativePath":"usage.md","filePath":"usage.md"}'),n={name:"usage.md"};function p(h,s,l,r,c,d){return t(),e("div",null,s[0]||(s[0]=[i("",31)]))}const g=a(n,[["render",p]]);export{k as __pageData,g as default};
